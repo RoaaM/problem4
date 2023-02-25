bca7db176823ec246697cd6c98d02e27efd2d580
RoaaM <roaa.sar1999@gmail.com>
2023-02-25 04:41:51 +0300

    init commit

---
+++ ImageStatistics.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;  // this allow us to use bitmap class
+
+
+namespace ProblemFour
+{
+    public class ImageStatistics
+    {
+        /* here we define a method called calculate wich takes a bitmap object
+           as a parameter and return a tuple containing four values: mean, std, min, max
+        */
+        public static (double mean, double stdDev, int minGrayValue, int maxGrayValue) Calculate(Bitmap image)
+        {
+            // here we get the dimention of the image
+            int width = image.Width;
+            int height = image.Height;
+
+
+            // initialize some value we will use it to calculate the image statistics
+            
+            double sumGrayValues = 0;  //is the sum of all gray values in the image 
+            double squaredSumGrayValues = 0;  //is the sum of the squared gray values in the image
+            int minGrayValue = 255;  //is the minimum gray value found in the image (0)
+            int maxGrayValue = 0;   //is the maximum gray value found in the image (255)
+
+
+            // this will loop through every pixel in the image and calculate the image statistics
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    int grayValue = pixelColor.R; // grayscale image, so R, G, and B are all equal
+
+                    sumGrayValues += grayValue;
+                    squaredSumGrayValues += grayValue * grayValue;
+
+                    if (grayValue < minGrayValue)
+                    {
+                        minGrayValue = grayValue;
+                    }
+
+                    if (grayValue > maxGrayValue)
+                    {
+                        maxGrayValue = grayValue;
+                    }
+                }
+            }
+
+            // and here we will calculate the final image statistics and return all values in a tuple
+            int totalPixels = width * height;
+            double meanGrayValue = sumGrayValues / totalPixels;
+            double stdDevGrayValue = Math.Sqrt((squaredSumGrayValues / totalPixels) - (meanGrayValue * meanGrayValue));
+
+            return (meanGrayValue, stdDevGrayValue, minGrayValue, maxGrayValue);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ProblemFour;
+using System.Drawing;
+
+
+namespace YourProjectName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create instance(object) of Bitmap class and give it the image
+            Bitmap image = new Bitmap("C:\\Users\\roaas\\Desktop\\ProblemFour\\ProblemFour\\coalagray.jpeg");
+            /* This line calls the static Calculate method of the ImageStatistics class, passing the Bitmap object 
+             * created in the previous line as an argument. The resulting statistics are stored in a new variable named statistics.*/
+            var statistics = ImageStatistics.Calculate(image);
+            // and thats how i want my output look like
+            Console.WriteLine($"Mean gray value: {statistics.mean}");
+            Console.WriteLine($"Standard deviation: {statistics.stdDev}");
+            Console.WriteLine($"Minimum gray value: {statistics.minGrayValue}");
+            Console.WriteLine($"Maximum gray value: {statistics.maxGrayValue}");
+
+        }
+    }
+}
